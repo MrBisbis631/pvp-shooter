2f5ff232f483e17a19a724b82b18057daf89d059
Altbegz <shmuelbisberg.acc@gmail.com>
2024-12-20 00:56:26 +0200

    Refactor useKeyPress hook to manage multiple key states and prevent opposite direction inputs

--- frontend/src/hooks/useKeyPress.ts
+++ frontend/src/hooks/useKeyPress.ts
@@ -1,40 +1,50 @@
 import { useEffect, useState } from "react";
 
 export type KeysState = {
-  key?: string | null;
-  isKeyPressed: boolean;
-}
+  keys: Set<string>;
+};
 
-export function useKeyPress() {
+export function useKeyPress(onKeyPress?: (e: KeyboardEvent) => void) {
   const [keyPressed, setKeyPressed] = useState<KeysState>({
-    key: null,
-    isKeyPressed: false,
+    keys: new Set(),
   });
 
-  const handleKeyDown = (e: KeyboardEvent) => {
-    setKeyPressed(prev => ({
-      ...prev,
-      key: e.key,
-      isKeyPressed: true,
-    }));
-  };
-
-  const handleKeyUp = () => {
-    setKeyPressed({
-      key: null,
-      isKeyPressed: false,
-    });
-  };
-
   useEffect(() => {
+    // Add key to set when key is pressed
+    const handleKeyDown = (e: KeyboardEvent) => {
+      onKeyPress?.(e);
+
+      const newSet = new Set(keyPressed.keys).add(e.key);
+
+      // Prevent opposite directions
+      if (e.key === "w") newSet.delete("s");
+      if (e.key === "s") newSet.delete("w");
+      if (e.key === "a") newSet.delete("d");
+      if (e.key === "d") newSet.delete("a");
+
+      setKeyPressed((prev) => ({
+        keys: prev.keys.add(e.key),
+      }));
+    };
+
+    // Remove key from set when key is released
+    const handleKeyUp = (e: KeyboardEvent) => {
+      const newSet = new Set(keyPressed.keys);
+      newSet.delete(e.key);
+
+      setKeyPressed({
+        keys: newSet,
+      });
+    };
+
     window.addEventListener("keydown", handleKeyDown);
     window.addEventListener("keyup", handleKeyUp);
 
     return () => {
       window.removeEventListener("keydown", handleKeyDown);
       window.removeEventListener("keyup", handleKeyUp);
-    }
-  }, []);
+    };
+  }, [keyPressed, onKeyPress]);
 
   return keyPressed;
 }
