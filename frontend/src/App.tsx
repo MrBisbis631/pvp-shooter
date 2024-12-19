a4d464d054a818aae98734ccb59275793529db33
Altbegz <shmuelbisberg.acc@gmail.com>
2024-12-20 00:56:40 +0200

    Refactor App component to integrate PIXI.js and encapsulate game logic within the Game component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,44 +1,20 @@
-import { useEffect } from "react";
-import { socket } from "./socket";
-import { Stage, Sprite } from "@pixi/react";
-import { useKeyPress } from "./hooks/useKeyPress";
-import { useSocketSene } from "./hooks/useSocketScene";
+import * as PIXI from "pixi.js";
+import { Game } from "./Game";
+import { AppProvider } from "@pixi/react";
 
-const STAGE_WIDTH = 800;
-const STAGE_HEIGHT = 600;
+const app = new PIXI.Application({
+  width: 800,
+  height: 600,
+  backgroundColor: 0x1099bb,
+});
 
 function App() {
-  const { isKeyPressed, key } = useKeyPress();
-  const scene = useSocketSene();
-
-  useEffect(() => {
-    if (key && isKeyPressed) {
-      socket.emit("key-pressed", key);
-    }
-  }, [key, isKeyPressed]);
-
   return (
     <div>
       <div className="">
-        <Stage
-          className="mx-auto mt-24"
-          options={{ background: 0x1099bb }}
-          width={STAGE_WIDTH}
-          height={STAGE_HEIGHT}
-        >
-          <Sprite
-            scale={0.3}
-            image="https://pixijs.com/assets/flowerTop.png"
-            x={scene?.player.x || 100}
-            y={scene?.player.y || 100}
-          />
-          <Sprite
-            scale={0.3}
-            image="https://pixijs.com/assets/eggHead.png"
-            x={scene?.enemy.x || STAGE_WIDTH - 100}
-            y={scene?.enemy.y || STAGE_HEIGHT - 100}
-          />
-        </Stage>
+        <AppProvider value={app}>
+          <Game />
+        </AppProvider>
       </div>
     </div>
   );
